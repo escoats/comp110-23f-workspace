a8ec5c77f592355e2138d2ced41d34391e31e7b5
Elizabeth Coats <escoats@unc.edu>
2023-09-23 23:02:13 -0400

    troubleshooting EX03

--- exercises/ex03_wordle.py
+++ exercises/ex03_wordle.py
@@ -2,8 +2,9 @@
 
 __author__ = "730659395"
 
+
 def contains_char(secret: str, char: str) -> bool:
-    """Recieves the secret word and one character, and checks to see if the char is found anywhere within the word. Returns True or False"""
+    """Recieves the secret word and one character, and checks to see if the char is found anywhere within the word. Returns True or False."""
     assert len(char) == 1
     i: int = 0
     while i < len(secret):
@@ -13,6 +14,7 @@ def contains_char(secret: str, char: str) -> bool:
     # If this point is reached, the character has not been found anywhere in the word
     return False
 
+
 def emojified(guess: str, secret: str) -> str:
     """Recieves the user's guess and the secret word, and returns an emoji display corresponding to the correctness of the guess."""
     assert len(guess) == len(secret)
@@ -21,34 +23,42 @@ def emojified(guess: str, secret: str) -> str:
     GREEN_BOX: str = "\U0001F7E9"
     YELLOW_BOX: str = "\U0001F7E8"
     WHITE_BOX: str = "\U00002B1C"
+   
     while i < len(secret):
         char_present: bool = contains_char(secret, guess[i])
         # Character is found somewhere in the word
-        if char_present == True:
+        if char_present is True:
             if guess[i] == secret[i]:
                 display += GREEN_BOX
             else: 
                 display += YELLOW_BOX
         # Character is not in the word
-        if char_present == False:
+        if char_present is False:
             display += WHITE_BOX
         i += 1
     return display
 
+
 def input_guess(expected_length: int) -> str:
     """Recieves the length of the secret word and asks the user to guess a word. Returns the guessed word."""
     guess: str = input(f"Input a {str(expected_length)}-character word: ")
-    while expected_length != len(guess):
+    # Guess is the right length
+    if len(guess) == expected_length:
+        return guess
+    # Guess is the wrong length
+    while len(guess) != expected_length:
         guess = input(f"That wasn't {expected_length} chars! Try again: ")
     return guess
+
 
 def main() -> None:
     """The entrypoint of the program and main game loop."""
     playing: bool = True
     turn_num: int = 1
     secret = "codes"
+    
     # Loops through turns
-    while playing == True and turn_num <= 6:
+    while playing is True and turn_num <= 6:
         print(f"=== Turn {turn_num}/6 ===")
         guess = input_guess(len(secret))
         display = emojified(guess, secret)
@@ -56,13 +66,13 @@ def main() -> None:
         # Checks if player has won
         if guess == secret:
             print("You won!")
-            playing == False
-            exit()
+            playing = False
         # Checks if player has lost
         if turn_num == 6:
             print("X/6 - Sorry, try again tomorrow!")
             exit()
         turn_num += 1
+
 
 if __name__ == "__main__":
     main()
