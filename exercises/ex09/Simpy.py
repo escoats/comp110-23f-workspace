41b99d6bfbfcb52d7e2886d96fed55b4ade03d7b
Elizabeth Coats <escoats@unc.edu>
2023-11-16 22:59:39 -0500

    SIMPY DONEEE

--- exercises/ex09/Simpy.py
+++ exercises/ex09/Simpy.py
@@ -8,13 +8,14 @@ __author__ = "730659395"
 
 
 class Simpy:
+    """Simpy class!"""
     values: list[float]
 
     def __init__(self, input_values: list[float]):
         """Constructor."""
         self.values = input_values
 
-    def __str__(self):
+    def __str__(self) -> str:
         """Creates a str representation of Simpy object."""
         return f"Simpy({self.values})"
     
@@ -25,37 +26,40 @@ class Simpy:
             self.values.append(val)
 
     def arange(self, start: float, stop: float, step: float = 1.0) -> None:
-        """Fills the values attribute with a range of values"""
+        """Fills the values attribute with a range of values."""
         assert step != 0.0
         self.values = []
         current_val: float = start
-        num_appends: int = int((stop - start)/step)
-        for item in range(0, num_appends):
-            self.values.append(current_val)
-            current_val += step
-        print(self.values)
+        if step > 0:
+            while current_val < stop:
+                self.values.append(current_val)
+                current_val += step
+        else:
+            while current_val > stop:
+                self.values.append(current_val)
+                current_val += step
     
     def sum(self) -> float:
-        """Returns a sum of a Simpy's values"""
+        """Returns a sum of a Simpy's values."""
         list_sum = sum(self.values)
         return list_sum
     
-    def __add__(self, rhs: float | Simpy) -> Simpy:
+    def __add__(self, rhs: Union[float, Simpy]) -> Simpy:
         """Overloads the addition operator and returns a new Simpy object."""
         new_simpy_vals: list = []
-        if type(rhs) == float:
+        if isinstance(rhs, float):
             for i in range(0, len(self.values)):
                 new_simpy_vals.append(rhs + self.values[i])
         else:
             assert len(self.values) == len(rhs.values)
-            for i in range(0,len(self.values)):
+            for i in range(0, len(self.values)):
                 new_simpy_vals.append(rhs.values[i] + self.values[i])
         return Simpy(new_simpy_vals)
 
-    def __pow__(self, rhs: float | Simpy) -> Simpy:
+    def __pow__(self, rhs: Union[float, Simpy]) -> Simpy:
         """Overloads the power operator and returns a new Simpy object."""
         new_simpy_vals: list = []
-        if type(rhs) == float:
+        if isinstance(rhs, float):
             for i in range(0, len(self.values)):
                 new_simpy_vals.append(self.values[i] ** rhs)
         else:
@@ -64,3 +68,51 @@ class Simpy:
                 new_simpy_vals.append(self.values[i] ** rhs.values[i])
         return Simpy(new_simpy_vals)
     
+    def __eq__(self, rhs: Union[float, Simpy]) -> list[bool]:
+        """Overloads the == operator."""
+        output: list[bool] = []
+        if isinstance(rhs, float):
+            for i in range(0, len(self.values)):
+                if self.values[i] == rhs:
+                    output.append(True)
+                else:
+                    output.append(False)
+        else:
+            assert len(self.values) == len(rhs.values)
+            for i in range(0, len(self.values)):
+                if self.values[i] == rhs.values[i]:
+                    output.append(True)
+                else:
+                    output.append(False)
+        return output
+
+    def __gt__(self, rhs: Union[float, Simpy]) -> list[bool]:
+        """Overloads the greater than operator."""
+        output: list[bool] = []
+        if isinstance(rhs, float):
+            for i in range(0, len(self.values)):
+                if self.values[i] > rhs:
+                    output.append(True)
+                else:
+                    output.append(False)
+        else:
+            assert len(self.values) == len(rhs.values)
+            for i in range(0, len(self.values)):
+                if self.values[i] > rhs.values[i]:
+                    output.append(True)
+                else:
+                    output.append(False)
+        return output
+    
+    def __getitem__(self, rhs: Union[int, list[bool]]) -> Union[float, Simpy]:
+        """Overloads subscription notation to filter with masks."""
+        if isinstance(rhs, int):
+            item: float = self.values[rhs]
+            return item
+        else:
+            new_simpy_vals: list[float] = []
+            for i in range(0, len(self.values)):
+                if rhs[i] is True:
+                    new_simpy_vals.append(self.values[i])
+            new_simpy: Simpy = Simpy(new_simpy_vals)
+            return new_simpy
--- lessons/recursion/node.py
+++ lessons/recursion/node.py
@@ -2,6 +2,8 @@
 
 from __future__ import annotations
 
+__author__ = "730659395"
+
 
 class Node:
     """My Node class for linked lists."""
@@ -12,7 +14,7 @@ class Node:
     def __init__(self, data: int, next: Node | None):
         """Construct Node."""
         self.data = data
-        self. next = next
+        self.next = next
         
     def __str__(self) -> str:
         """Produce a string visualization of the linked list."""
@@ -22,11 +24,21 @@ class Node:
         else:
             return f"{self.data} -> {self.next}"
         
-    def head(self):
-        return None
+    def head(self) -> int:
+        """Returns the data attribute for the first element in the linked list."""
+        return self.data
     
-    def tail(self):
-        return None
+    def tail(self) -> Node | None:
+        """Returns a linked list of every element minus the head."""
+        if self.next is None:
+            return None
+        else: 
+            return self.next
     
-    def last(self):
-        return None
+    def last(self) -> int:
+        """Returns the data attribute for the last element in the linked list."""
+        while True:
+            if self.next.next is not None:
+                self.next = self.next.next
+            else:
+                return self.next.data
