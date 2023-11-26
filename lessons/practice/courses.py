4ba1cf51fa904bca02e56912c88d965f88f4f52a
Elizabeth Coats <escoats@unc.edu>
2023-11-26 17:18:26 -0500

    worked on quiz practice?

--- lessons/practice/courses.py
+++ lessons/practice/courses.py
@@ -9,11 +9,13 @@ class Course:
     level: int
     prerequisites: list[str]
 
+    '''
     def __init__(self, init_name, init_level, init_prereqs):
         self.name = init_name
         self.level = init_level
         self.prerequisites = init_prereqs
     
+        '''
     def is_valid_self(self, prereq: str) -> bool:
         if (self.level >= 400) and (prereq in self.prerequisites):
             return True
